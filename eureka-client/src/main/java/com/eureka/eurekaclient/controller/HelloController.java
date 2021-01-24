563fc2ce3c2dc67bf2a3d0bcb4eb396eadeb95e3
JsonTom888 <Good666888@163.com>
2021-01-24 22:48:46 +0800

    eureka基础代码

---
+++ eureka-client/src/main/java/com/eureka/eurekaclient/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.eureka.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tom
+ * @version V1.0
+ * @date 2021/1/24 22:36
+ */
+@RestController
+public class HelloController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home(@RequestParam(value = "name", defaultValue = "jsontom") String name) {
+        return "hello " + name + " ,i am from port:" + port;
+    }
+
+}
